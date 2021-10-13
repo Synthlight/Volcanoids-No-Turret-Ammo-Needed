bdc119e9383c63fb93fe85a00c7ed28deb6bde63
Gregory Brutsche <gregory@mail.brutsches.local>
2021-10-13 15:45:39 -0400

    Final touches for the workshop update.

--- Plugin.cs
+++ Plugin.cs
@@ -4,7 +4,6 @@ using JetBrains.Annotations;
 namespace No_Turret_Ammo_Needed {
     [UsedImplicitly]
     public class Plugin : BaseGameMod {
-        protected override string ModName    => "No-Turret-Ammo-Needed";
-        protected override bool   UseHarmony => true;
+        protected override bool UseHarmony => true;
     }
 }
